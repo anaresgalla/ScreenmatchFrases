28ab1e0715d84bccb567d5a944275eb256d5436a
anaresgalla <anaresgalla@gmail.com>
2026-01-12 16:55:13 -0300

    Cria Frase Service e Frase Repository

--- src/main/java/br/com/alura/screenmatch_frases/FraseService.java
+++ src/main/java/br/com/alura/screenmatch_frases/FraseService.java
@@ -1,7 +1,16 @@
 package br.com.alura.screenmatch_frases;
 
-public class FraseService {
-    public FraseDTO obterFraseAleatoria() {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class FraseService {
+
+    @Autowired
+    private FraseRepository repositorio;
+
+    public FraseDTO obterFraseAleatoria() {
+        Frase frase = repositorio.findById(1L).get();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
     }
 }
